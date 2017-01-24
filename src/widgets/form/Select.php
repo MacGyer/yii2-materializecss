0f85f3f8ed9a41c4a982c82a43c9845a4a8179f9
Christoph Erdmann <cerdmann@medienpol.de>
2017-01-24 22:18:58 +0100

    select placeholder option

--- src/widgets/form/Select.php
+++ src/widgets/form/Select.php
@@ -4,14 +4,27 @@ namespace common\widgets;
 
 use macgyer\yii2materializecss\lib\BaseInputWidget;
 use macgyer\yii2materializecss\lib\Html;
+use yii\helpers\ArrayHelper;
 
 class Select extends BaseInputWidget
 {
     public $items = [];
 
+    public $options = [];
+
     public function init()
     {
         parent::init();
+
+        $placeholder = ArrayHelper::remove($this->options, 'placeholder', 'Please choose');
+
+        if ($placeholder !== false) {
+            $this->items = ArrayHelper::merge(['' => $placeholder], $this->items);
+
+            $this->options['options'] = [
+                '' => ['disabled' => true, 'selected' => true],
+            ];
+        }
 
         $this->registerPlugin('material_select');
     }
@@ -23,5 +36,7 @@ class Select extends BaseInputWidget
         } else {
             $html = Html::dropDownList($this->name, $this->value, $this->items, $this->options);
         }
+
+        return $html;
     }
 }
