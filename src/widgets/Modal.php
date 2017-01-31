d0e8f0bdf5a315f0477f2ff7c4405bcc0b32366f
Christoph Erdmann <cerdmann@medienpol.de>
2017-01-31 23:57:18 +0100

    FAB added toolbar support

--- src/widgets/Modal.php
+++ src/widgets/Modal.php
@@ -14,9 +14,9 @@ use yii\helpers\ArrayHelper;
 /**
  * Modal renders a modal window that can be toggled by clicking on a button.
  *
- * The following example will show the content enclosed between the 
- * [\yii\base\Widget::begin()](http://www.yiiframework.com/doc-2.0/yii-base-widget.html#begin()-detail) and 
- * [\yii\base\Widget::end()](http://www.yiiframework.com/doc-2.0/yii-base-widget.html#end()-detail) calls within the 
+ * The following example will show the content enclosed between the
+ * [\yii\base\Widget::begin()](http://www.yiiframework.com/doc-2.0/yii-base-widget.html#begin()-detail) and
+ * [\yii\base\Widget::end()](http://www.yiiframework.com/doc-2.0/yii-base-widget.html#end()-detail) calls within the
  * modal window:
  *
  * ```php
