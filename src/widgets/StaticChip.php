0b1e8142130969f053c753f2e21b90d016acadb7
Christoph Erdmann <cerdmann@matero.de>
2018-04-02 11:37:46 +0200

    breadcrumbs

--- src/widgets/StaticChip.php
+++ src/widgets/StaticChip.php
@@ -15,7 +15,7 @@ use yii\helpers\ArrayHelper;
 /**
  * StaticChip renders small units of information.
  *
- * An Usual use case is the displaying of contact information.
+ * An usual use case is the displaying of contact information.
  * If you need tagging support in forms or inputs generally, please use [[\macgyer\yii2materializecss\widgets\form\ChipInput|ChipInput]].
  *
  * @author Christoph Erdmann <yii2-materializecss@pluspunkt-coding.de>
